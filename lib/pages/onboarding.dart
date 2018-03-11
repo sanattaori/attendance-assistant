2a3b50e1f1d82e0810252af5deb4b97e7f84b71d
sanattaori <sanattaori3@gmail.com>
2018-03-11 13:52:33 +0530

    init onboarding

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      
+    );
+  }
+
+}
