93cd97818facbe15a12c0623c3720c6075b4c131
sanattaori <sanattaori3@gmail.com>
2018-03-11 15:45:32 +0530

    onboarding

--- lib/pages/ob_pages/pages.dart
+++ lib/pages/ob_pages/pages.dart
@@ -1,8 +1,111 @@
 import 'package:flutter/material.dart';
+//TODO : (Ravi) Modify new assets images and hero icons
+final pages = [
+  new PageViewModel(
+      const Color(0xFF678FB4),
+      'assets/ob/img_ob_att.png',
+      'Transforming Attendance System',
+      'Now taking Attendance made easy, Store and manage Attendance data securly stored in digital account',
+      'assets/ob/img_ob_att.png'
+  ),
+  new PageViewModel(
+      const Color(0xFF65B0B4),
+      'assets/ob/img_ob_att.png',
+      'Build for Students and Teachers',
+      'Directly Share Class Notes and materials, helping student share code and get guidance',
+      //TODO : add more content
+      'assets/ob/img_ob_att.png'
+  ),
+  new PageViewModel(
+    const Color(0xFF9B90BC),
+    'assets/ob/img_ob_att.png',
+    'Easily Track attendance',
+    'Detailed Analysis made and Statics report avaliable even save as xml offine',
+    'assets/ob/img_ob_att.png',
+  ),
+];
 
 class Page extends StatelessWidget {
+
+  final PageViewModel viewModel;
+  final double percentVisible;
+
+  Page({
+    this.viewModel,
+    this.percentVisible = 1.0,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new Container(
+      padding: new EdgeInsets.all(25.0),
+        width: double.INFINITY,
+        color: viewModel.color,
+        child: new Opacity(
+          opacity: percentVisible,
+          child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 50.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(bottom: 25.0),
+                    child: new Image.asset(
+                        viewModel.heroAssetPath,
+                        width: 200.0,
+                        height: 200.0
+                    ),
+                  ),
+                ),
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: new Text(
+                      viewModel.title,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'FlamanteRoma',
+                        fontSize: 34.0,
+                      ),
+                    ),
+                  ),
+                ),
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(bottom: 75.0),
+                    child: new Text(
+                      viewModel.body,
+                      
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ]
+          ),
+        )
+    );
   }
+}
+
+class PageViewModel {
+  final Color color;
+  final String heroAssetPath;
+  final String title;
+  final String body;
+  final String iconAssetPath;
+
+  PageViewModel(
+      this.color,
+      this.heroAssetPath,
+      this.title,
+      this.body,
+      this.iconAssetPath,
+      );
 }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,61 +1,115 @@
+import 'dart:async';
+
+import 'package:attendance_assistant/pages/ob_pages/page_dragger.dart';
+import 'package:attendance_assistant/pages/ob_pages/page_reveal.dart';
+import 'package:attendance_assistant/pages/ob_pages/pager_indicator.dart';
+import 'package:attendance_assistant/pages/ob_pages/pages.dart';
 import 'package:flutter/material.dart';
 
 class Onboarding extends StatefulWidget {
+
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<Onboarding> {
+class _MyHomePageState extends State<Onboarding> with TickerProviderStateMixin {
+
+  StreamController<SlideUpdate> slideUpdateStream;
+  AnimatedPageDragger animatedPageDragger;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _MyHomePageState() {
+    slideUpdateStream = new StreamController<SlideUpdate>();
+
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.dragging) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if (slideDirection == SlideDirection.leftToRight) {
+            nextPageIndex = activeIndex - 1;
+          } else if (slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          } else {
+            nextPageIndex = activeIndex;
+          }
+        } else if (event.updateType == UpdateType.doneDragging) {
+          print('Done dragging.');
+          if (slidePercent > 0.5) {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.open,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+          } else {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.close,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+
+            nextPageIndex = activeIndex;
+          }
+
+          animatedPageDragger.run();
+        } else if (event.updateType == UpdateType.animating) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+        } else if (event.updateType == UpdateType.doneAnimating) {
+          print('Done animating. Next page index: $nextPageIndex');
+          activeIndex = nextPageIndex;
+
+          slideDirection = SlideDirection.none;
+          slidePercent = 0.0;
+
+          animatedPageDragger.dispose();
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Stack(
-        children: <Widget>[
-          new Container(
-            width: double.infinity,
-            color: Colors.blue,
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Padding(
-                    padding: new EdgeInsets.only(bottom: 25.0)),
-
-                new Icon(Icons.person ,size: 50.0,),
-                new Padding(
-                    padding: new EdgeInsets.only(bottom: 25.0)),
-
-                new Text(
-                  'Transforming Attendance System',
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(
-                    color: Colors.white,
-
-                    fontFamily: 'FlamanteRoma',
-                    fontSize: 34.0,
-
-                  ),
-                ),
-                new Padding(
-                    padding: new EdgeInsets.only(bottom: 10.0,top: 10.0)),
-
-                new Text(
-                  'Store Attendance data securely in your digital account',
-                    textAlign: TextAlign.center,
-                    style: new TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-
-                  ),
-                ),
-                new Padding(padding: new EdgeInsets.only(bottom: 85.0))
-
-              ],
+        children: [
+          new Page(
+            viewModel: pages[activeIndex],
+            percentVisible: 1.0,
+          ),
+          new PageReveal(
+            revealPercent: slidePercent,
+            child: new Page(
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
             ),
           ),
-
+          new PagerIndicator(
+            viewModel: new PagerIndicatorViewModel(
+              pages,
+              activeIndex,
+              slideDirection,
+              slidePercent,
+            ),
+          ),
+          new PageDragger(
+            canDragLeftToRight: activeIndex > 0,
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            slideUpdateStream: this.slideUpdateStream,
+          ),
         ],
       ),
     );
   }
-
 }
