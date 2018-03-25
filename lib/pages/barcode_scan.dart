5f58ba3a087c0f2e63927efe9cc6399232242511
Sanat Taori <sanattaori@gmail.com>
2018-03-25 15:44:37 +0530

    scan fix

--- lib/pages/barcode_scan.dart
+++ lib/pages/barcode_scan.dart
@@ -4,7 +4,6 @@ import 'package:attendance_assistant/pages/live_help.dart';
 import 'package:attendance_assistant/pages/qr_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 
 
@@ -117,23 +116,23 @@ class BarcodeScanState extends State<BarcodeScan> with TickerProviderStateMixin{
   }
 
 
-  Future scan() async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      setState(() => this.barcode = barcode);
-    } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          this.barcode = 'The user did not grant the camera permission!';
-        });
-      } else {
-        setState(() => this.barcode = 'Unknown error: $e');
-      }
-    } on FormatException{
-      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
-    } catch (e) {
-      setState(() => this.barcode = 'Unknown error: $e');
-    }
-  }
+//  Future scan() async {
+//    try {
+//      String barcode = await BarcodeScanner.scan();
+//      setState(() => this.barcode = barcode);
+//    } on PlatformException catch (e) {
+//      if (e.code == BarcodeScanner.CameraAccessDenied) {
+//        setState(() {
+//          this.barcode = 'The user did not grant the camera permission!';
+//        });
+//      } else {
+//        setState(() => this.barcode = 'Unknown error: $e');
+//      }
+//    } on FormatException{
+//      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
+//    } catch (e) {
+//      setState(() => this.barcode = 'Unknown error: $e');
+//    }
+//  }
 
 }
